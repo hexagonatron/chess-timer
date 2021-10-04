c86a2527b530265939579ccd8c2a74131b68fa4e
Ben <46476247+hexagonatron@users.noreply.github.com>
2021-10-04 14:28:43 +1030

    Adding button functionality.

--- chess-timer.ino
+++ chess-timer.ino
@@ -1,10 +1,22 @@
+const int btn1Pin = 2;
+const int btn2Pin = 3;
+
 const long millisInSec = 1000UL;
 const long millisInMin = millisInSec * 60;
 const long millisInHour = millisInMin * 60;
 
+const long debounceTime = 50UL;
+
+const int btnNum = 2;
+int btnPins[btnNum] = {btn1Pin, btn2Pin};
+bool currBtnState[btnNum] = {false, false};
+bool lastBtnState[btnNum] = {false, false};
+long lastBtnTime[btnNum] = {0UL, 0UL};
+
 void setup() {
     Serial.begin(115200);
-
+    pinMode(btn1Pin, INPUT_PULLUP);
+    pinMode(btn2Pin, INPUT_PULLUP);
 }
 
 long playerOneTime = 1000UL*62;
@@ -15,16 +27,53 @@ long playerMillis[] =  {playerOneTime, playerTwoTime};
 long startTime = 0UL;
 long lastMillis = 0UL;
 
-int activePlayer = 1;
+int activePlayer = 0;
 
 bool isRunning = false;
 
 void loop() {
     start();
-    display();
     updateTime();
+    checkBtns();
+    display();
     delay(50);
+}
 
+void checkBtns() {
+    debounceBtns();
+    updateActivePlayer();
+}
+
+void updateActivePlayer() {
+    if (activePlayer == 0) {
+        if (isNewBtnPushDown(0)) {
+            activePlayer = 1;
+        }
+    } else if (activePlayer == 1) {
+        if (isNewBtnPushDown(1)) {
+            activePlayer = 0;
+        }
+    }
+}
+
+bool isNewBtnPushDown(int btnIndex) {
+    return currBtnState[btnIndex] && !lastBtnState[btnIndex];
+}
+
+void debounceBtns() {
+    for (int i = 0; i < btnNum; i++) {
+        debounceBtn(i);
+    }
+}
+
+void debounceBtn(int btnIndex) {
+        bool currState = digitalRead(btnPins[btnIndex]);
+        long currTime = millis();
+        if (currTime - lastBtnTime[btnIndex] > debounceTime) {
+            lastBtnState[btnIndex] = currBtnState[btnIndex];
+            currBtnState[btnIndex] = currState;
+            lastBtnTime[btnIndex] = currTime;
+        }
 }
 
 void start() {
